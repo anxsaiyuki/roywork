c3dd6b6d1a167930dee6d8799c27c6702a6a3199
anxsaiyuki <anxsaiyuki@gmail.com>
2014-11-10 17:08:15 +0800

    email api

--- app/controllers/roy_controller.rb
+++ app/controllers/roy_controller.rb
@@ -2,6 +2,7 @@ class RoyController < ApplicationController
 respond_to :json
     
   def royemail
+      p params
       UserMailer.welcome_email("anxsaiyuki@gmail.com").deliver
       render :json => {:name => "David"}.to_json
       
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,12 @@
 class UserMailer < ActionMailer::Base
   default from: "5dteamcore@gmail.com"
 
-  def welcome_email(user)
+  def welcome_email(name,number,email,content)
         @user = "5dteamcore@gmail.com"
+        @name = mame
+        @number = number
+        @email = email
+        @content = content
         mail(to: @user, subject: 'Welcome to my site')
   end
 
