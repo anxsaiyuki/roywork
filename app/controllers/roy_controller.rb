c66c7b1987078da57e783491784ae9b01cd66530
anxsaiyuki <anxsaiyuki@gmail.com>
2014-11-10 17:12:26 +0800

    finish email argument

--- app/controllers/roy_controller.rb
+++ app/controllers/roy_controller.rb
@@ -3,7 +3,7 @@ respond_to :json
     
   def royemail
       p params
-      UserMailer.welcome_email("anxsaiyuki@gmail.com").deliver
+      UserMailer.welcome_email(params[:name],params[:phone],params[:email],params[:content]).deliver
       render :json => {:name => "David"}.to_json
       
   end
