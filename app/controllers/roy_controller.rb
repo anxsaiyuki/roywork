c87dfba45d3fe7034a873cf40d3991afbb41c63d
anxsaiyuki <anxsaiyuki@gmail.com>
2014-11-10 15:50:09 +0800

    test4

--- app/controllers/roy_controller.rb
+++ app/controllers/roy_controller.rb
@@ -3,10 +3,9 @@ respond_to :json
     
   def royemail
       UserMailer.welcome_email("anxsaiyuki@gmail.com").deliver
-
-      respond_to do |format|
-            format.json { render json: {:msg => 'has this user'} }
-      end
+      render :json = {
+        :test => "good"
+          }
       
   end
 
